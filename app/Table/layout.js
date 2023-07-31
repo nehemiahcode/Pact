8cc354a999d7d8af6a472e126179910aa5f7d079
nehemiahcode <codehubby@gmail.com>
2023-08-01 00:50:12 +0100

    fourth commit

--- app/Table/layout.js
+++ app/Table/layout.js
@@ -6,9 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
         <body>{children}</body>
-      </head>
     </html>
   );
 }
