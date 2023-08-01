c778ac86c26588257da309cdc7ead90fe0f7fb4b
nehemiahcode <codehubby@gmail.com>
2023-08-01 18:11:10 +0100

    sixt commit

--- app/home/page.js
+++ app/home/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Hero from "../Components/Hero";
 import Section1 from "../Components/Section1";
 import Section2 from "../Components/Section2";
@@ -7,29 +7,11 @@ import Sections3 from "../Components/Sections3";
 import Tabs from "../Components/Tabs";
 import Getstarted from "../Components/Getstarted";
 import Mainlayout from "../Components/Mainlayout";
-import { BiLogoPatreon } from "react-icons/bi";
 
 function Page() {
-  const [loading, setLoading] = useState(true);
-  
-  useEffect(() => {
-    const Timer = setTimeout(() => {
-        setLoading(false)
-    }, 2000)
-    return () => clearTimeout(Timer)
-  })
+
   return (
-    <>
-      {loading ? (
-        <div className="h-screen w-screen fixed top-0 bottom-0 bg-slate-900 flex items-center justify-center">
-          <div className=" animate-pulse flex items-center justify-normal gap-2">
-        <span className=" text-pink-400 text-5xl">
-          <BiLogoPatreon />
-        </span>
-        <h1 className=" text-white text-2xl font-bold">Packt</h1>
-      </div>
-        </div>
-      ) : (
+    <>  
         <Mainlayout>
           <Hero />
           <Section1 />
@@ -38,7 +20,7 @@ function Page() {
           <Tabs />
           <Getstarted />
         </Mainlayout>
-      )}
+   
     </>
   );
 }
